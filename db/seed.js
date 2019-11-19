388551fd3aa24db68992ae09edfb131a8344f78c
DallasLind <mcgraw.dallas@gmail.com>
2019-11-19 11:26:53 -0500

    No nesting

--- db/seed.js
+++ db/seed.js
@@ -1,22 +1,7 @@
-const gamesJSON = require("./games");
-const Games = require("../models/game");
 const Meetup = require("../models/meetup")
 const meetupJSON = require("./meetup");
 
-const gamesData = gamesJSON.map(item => {
-    const games = {}
-    games.name = item.name
-    games.id = item.id
-    games.image_url = item.image_url
-    games.year_published = item.year_published
-    games.min_players = item.min_players
-    games.max_players = item.max_players
-    games.game_url = item.game_url
-    games.description = item.description
-    return games
-})
-
-    const meetupData = meetupJSON.map(item => {
+const meetupData = meetupJSON.map(item => {
     const meetups = {}
     meetups.location = item.location
     meetups.time = item.time
@@ -24,7 +9,7 @@ const gamesData = gamesJSON.map(item => {
     meetups.creator = item.creator
     meetups.description = item.description
     meetups.players = item.players
-    meetups.game = item.name
+    meetups.game = item.game
     return meetups
 })
 
@@ -41,16 +26,4 @@ Meetup.deleteMany({})
     })
 })
 
-Games.deleteMany({})
-    .then(() => {
-        Games.create(gamesData)
-        .then(games => {
-            console.log(games)
-            process.exit();
-        })
-        .catch(err => {
-            console.log(err)
-            process.exit();
-        })
-    })
 
