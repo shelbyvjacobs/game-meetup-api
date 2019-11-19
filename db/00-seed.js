98ca951aa98d090dd676e7eebc1c4c99d4c7bb4c
DallasLind <mcgraw.dallas@gmail.com>
2019-11-19 11:02:24 -0500

    JSON separated

--- db/00-seed.js
+++ db/00-seed.js
@@ -1,7 +1,5 @@
 const gamesJSON = require("./games");
 const Games = require("../models/game");
-const Meetup = require("../models/meetup")
-const meetupJSON = require("./meetup");
 
 const gamesData = gamesJSON.map(item => {
     const games = {}
@@ -16,31 +14,6 @@ const gamesData = gamesJSON.map(item => {
     return games
 })
 
-    const meetupData = meetupJSON.map(item => {
-    const meetups = {}
-    meetups.location = item.location
-    meetups.time = item.time
-    meetups.date = item.date
-    meetups.creator = item.creator
-    meetups.description = item.description
-    meetups.players = item.players
-    meetups.game = item.name
-    return meetups
-})
-
-Meetup.deleteMany({})
-.then(() => {
-    Meetup.create(meetupData)
-    .then(meetup => {
-        console.log(meetup)
-        process.exit();
-    })
-    .catch(err => {
-        console.log(err)
-        process.exit();
-    })
-})
-
 Games.deleteMany({})
     .then(() => {
         Games.create(gamesData)
@@ -53,4 +26,3 @@ Games.deleteMany({})
             process.exit();
         })
     })
-
